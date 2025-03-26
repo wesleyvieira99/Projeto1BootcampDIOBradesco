35fe62a3da024fd816ddca66de3cbdb394c51e61
Wesley Vieira <75049150+wesleyvieira99@users.noreply.github.com>
2025-03-26 15:42:42 -0300

    Update and rename DesafioControleFluxo to DesafioControleFluxo.java

--- DesafioControleFluxo.java
+++ DesafioControleFluxo.java
@@ -12,20 +12,20 @@ public class Contador {
 		
 		}catch (ParametrosInvalidosException exception) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-      System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+      			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-    if (parametroUm > parametroDois) {
-      throw new ParametrosInvalidosException();
-    }
+		    if (parametroUm > parametroDois) {
+		      throw new ParametrosInvalidosException();
+		    }
 		
 		int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
-    for (int i=0; i <= contagem; i++) {
-        System.out.println("Imprimindo o número " + i);
-    }
+		    for (int i=0; i <= contagem; i++) {
+		        System.out.println("Imprimindo o número " + i);
+		    }
 	}
 }
